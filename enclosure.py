d612f265cbdb08df5b537288ccc589356684b6a5
amylukan <lukay008@mymail.unisa.edu.au>
2025-11-21 11:56:48 +0800

    validation and attributes

--- enclosure.py
+++ enclosure.py
@@ -6,10 +6,17 @@ ID:
 Username: LUKAY008
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+from animal import Animal
 
 class Enclosure:
     """Houses animals of one species and one environment."""
     def __init__(self, name, size_m2, environment_type):
+        if size_m2 <= 0:
+            raise ValueError('Size must be greater than zero.')
         self.name = name
         self.size_m2 = size_m2
-        self.environment_type = environment_type
+        self.environment_type = environment_type.lower()
+        self.cleanliness = 80
+        self.animals = []
+        self.species_restriction = None
+
